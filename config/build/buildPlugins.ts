52749b25afcf442f2d870ba679d0b267454439f0
Baratov Viktor <vitya.baratov@gmail.com>
2025-09-11 23:46:52 +0300

    Initial commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,32 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack, { Configuration } from "webpack";
+import { BuildOptions } from "./types/types";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+
+export function buildPlugins({mode, paths}: BuildOptions): Configuration["plugins"] {
+    const isDev = mode === "development";
+    const isProd = !isDev;
+
+    const plugins: Configuration["plugins"] = [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+            favicon: path.resolve(paths.public, 'favicon.svg')
+          })
+    ]
+
+    if(isDev) {
+        plugins.push(new webpack.ProgressPlugin())
+    }
+
+    if(isProd) {
+        plugins.push(new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+          }))
+    }
+
+
+
+    return plugins
+}
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import style from './App.module.scss';
+
+const App = () => {
+    return (
+        <div className={style.test}>
+            Hello World 
+        </div>
+    );
+};
+
+export default App;
