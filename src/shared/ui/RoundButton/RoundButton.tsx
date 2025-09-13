bfcba4e63b2dfec6bb0f2ba12f0616ee7b348e09
Baratov Viktor <vitya.baratov@gmail.com>
2025-09-13 22:01:17 +0300

    feat: добавил кнопку навигации

---
+++ src/shared/ui/RoundButton/RoundButton.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./RoundButton.module.scss";
+import LeftArrow from "@/assets/LeftArrow.svg";
+import RightArrow from "@/assets/RightArrow.svg";
+
+import { gsap } from "gsap";
+
+interface RoundButtonProps {
+  onClick?: () => void;
+  direction?: "<" | ">";
+  disabled?: boolean;
+}
+
+const RoundButton: React.FC<RoundButtonProps> = ({
+  onClick,
+  direction = ">",
+  disabled
+}) => {
+  return (
+    <div style={{display: 'flex'}}>
+      <button className={styles.button} onClick={onClick} disabled={disabled}>
+        {direction === "<" && <LeftArrow width= "10" height="14" viewBox="0 0 10 14"/>}
+        {direction === ">" && <RightArrow width= "10" height="14" viewBox="0 0 10 14"/>}
+      </button>
+    </div>
+  );
+};
+
+export default RoundButton;
