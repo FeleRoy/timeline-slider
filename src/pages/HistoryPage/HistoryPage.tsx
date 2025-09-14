0bfa715c1df7a31fed7a0a5fd11fc741fc810a03
Baratov Viktor <vitya.baratov@gmail.com>
2025-09-14 19:09:35 +0300

    feat: добавил title

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CircleButtons from "@/shared/ui/CircleButtons/CircleButtons";
 import CircleNav from "./ui/CircleNav/CircleNav";
 import styles from './HistoryPage.module.scss';
@@ -7,6 +7,8 @@ import YearsSlider from "@/shared/ui/YearsSlider/YearsSlider";
 import AnimatedDate from "@/shared/ui/AnimatedDate/AnimatedDate";
 import { timelineDate } from "@/utils/types";
 import { HistoryPageMock } from "./mock/mock";
+
+import gsap from "gsap";
 
 
 interface HistoryPageProps {
@@ -21,6 +23,18 @@ const HistoryPage: React.FC<HistoryPageProps> = ({
     console.log('Нажата кнопка с индексом:', index + 1);
     setCurrentPeriod(index + 1);
   };
+ const titleRef = useRef<HTMLHeadingElement | null>(null);
+
+    useEffect(() => {
+    if (titleRef.current) {
+      gsap.fromTo(
+        titleRef.current,
+        { opacity: 0},
+        { opacity: 1, duration: 0.8, ease: "power1.out" }
+      );
+    }
+  }, [currentPeriod]);
+
 
   return (
     <>
@@ -51,6 +65,7 @@ const HistoryPage: React.FC<HistoryPageProps> = ({
           setCurrentPeriod(currentPeriod + 1);
         }}
       ></CircleNav>
+      <h2 ref={titleRef} className={styles.title}>{timelines[currentPeriod - 1].title}</h2>
       </div>
       <div className={styles['slider-wrapper']}>
       <YearsSlider date={timelines[currentPeriod - 1].yearsDate}/>
